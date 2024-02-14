075ccf7ccb61dc83f6d5caaa3041e8d7643d6bd6
Ben <whippedcreamtoast@gmail.com>
2024-02-13 19:30:47 -0500

    initial commit!

---
+++ Bezier.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "Bezier.h"
+
+double Bezier::quadraticBezier(double p0, double p1, double p2, double t){
+
+    
+
+}
---
+++ Bezier.h
@@ -0,0 +1,9 @@
+
+class Bezier{
+
+    public:
+    double quadraticBezier(double p0, double p1, double p2, double t);
+
+    private:
+
+};
