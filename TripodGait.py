005743d3382d76aed50a6d6f7a053b6c5d1c2035
Ben Phan <66501019+LiterallyCanada312@users.noreply.github.com>
2024-03-07 18:53:41 -0500

    Starting first gait

--- TripodGait.py
+++ TripodGait.py
@@ -1,1 +1,18 @@
+from servo import Servo, servo2040
+from test import legs, moveTo
+from Vector import Vector3D
+import Bezier
+
+# Tripod Control Point Array
+
+TCPA = [
+    Vector3D(),
+    Vector3D(), 
+    Vector3D(),
+    Vector3D(),
+    Vector3D(),
+    Vector3D()
+]
+
+TIBCPA = [] # Tripod In Between Control Point Array 
 
