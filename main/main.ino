654b78ebf3f46a282cf021881042c6655f5cb531
Ben <whippedcreamtoast@gmail.com>
2024-04-13 21:26:41 -0400

    Finished proto pcb

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,6 @@
 #include <Servo.h>
-#include <nRF24L01>
 #include <SPI.h>
-  
+
 #include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
 
@@ -13,8 +12,13 @@
 
 #define pi 3.14159
 
+#define SERVOMIN //tbd 
+#define SERVOMAX //tbd
+
 Adafruit_PWMServoDriver pwm0 = Adafruit_PWMServoDriver(0x40);
 Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x41);
+
+int legs[16] = {};
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,7 +38,7 @@ void loop() {
     
 }
 
-void setAngle(int leg, Vector3 vec){
+void setLegToVector(int leg, Vector3 vec){
     float X = vec.x;
     float Y = vec.y;
     float Z = vec.z;
@@ -42,18 +46,29 @@ void setAngle(int leg, Vector3 vec){
     Y += Y_OFFSET;
     Z += Z_OFFSET;
 
-     J1 = math.atan(X/Y) * (180 / pi);
-     H = math.sqrt((Y * Y) + (X * X));
-     L = math.sqrt((H * H) + (Z * Z));
-     J3 = 360 - math.acos(constrain((((FEMUR_LENGTH * FEMUR_LENGTH) - (TIBIA_LENGTH * TIBIA_LENGTH) - (L * L))   /   (-2 * FEMUR_LENGTH * TIBIA_LENGTH)   ), -1, 1))*(180 / pi);
-     B = math.acos(constrain((((L * L) + (FEMUR_LENGTH * FEMUR_LENGTH) - (TIBIA_LENGTH * TIBIA_LENGTH))   /   (2 * L * FEMUR_LENGTH)   ), -1, 1)) * (180 / math.pi);
-     A = math.atan(Z / H) * (180 / pi);
-     J2 = (B + A);
+    float J1 = math.atan(X/Y) * (180 / pi);
+    float H = math.sqrt((Y * Y) + (X * X));
+    float L = math.sqrt((H * H) + (Z * Z));
+    float J3 = 360 - math.acos(constrain((((FEMUR_LENGTH * FEMUR_LENGTH) - (TIBIA_LENGTH * TIBIA_LENGTH) - (L * L))   /   (-2 * FEMUR_LENGTH * TIBIA_LENGTH)   ), -1, 1))*(180 / pi);
+    float B = math.acos(constrain((((L * L) + (FEMUR_LENGTH * FEMUR_LENGTH) - (TIBIA_LENGTH * TIBIA_LENGTH))   /   (2 * L * FEMUR_LENGTH)   ), -1, 1)) * (180 / math.pi);
+    float A = math.atan(Z / H) * (180 / pi);
+    float J2 = (B + A);
 
-    switch(leg):
-      case 1:
-        
+    if(leg != -1){
+      switch(leg){
 
+        case 1:
+
+          break;
+
+        case 2:
+          break
+
+      }
+    }
 
 }
+
+void setAngles(int leg, float value)
+
 
